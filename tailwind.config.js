84bf7cbe202fe460da5a76351eaff592b1d255cf
Thamer Ayachi <thamerjak3@gmail.com>
2022-10-27 11:19:06 +0100

    add navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
 	content: ["./public/**/*.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				lmb: "#fafafa",
+				tc: "#9698a6",
+				limeG: "#31d35c",
+				cyan: "#2bb7da",
+			},
+		},
 	},
 	plugins: [],
 };
